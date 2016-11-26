873ffea0cf7414c448d4cf4ca034ca2fcbd0e8bf
Oyvind Jonassen <oyjonass@hotmail.com>
2016-11-26 22:54:40 +0100

    Added AspNet.Identity, Web.Optimization and some basic Models

---
+++ wwwroot/Models/Blocks/SiteBlockData.cs
@@ -0,0 +1,9 @@
+namespace wwwroot.Models.Blocks
+{
+    /// <summary>
+    /// Base class for all block types on the site
+    /// </summary>
+    public abstract class SiteBlockData : EPiServer.Core.BlockData
+    {
+    }
+}
---
+++ wwwroot/Models/Pages/SitePageData.cs
@@ -0,0 +1,12 @@
+using EPiServer.Core;
+
+namespace wwwroot.Models.Pages
+{
+    /// <summary>
+    /// Base class for all page types
+    /// </summary>
+    public abstract class SitePageData : PageData
+    {
+
+    }
+}
